403cc9b578a625c34513a72139c9b8c9f0501057
Lucas Melo <lucasmelo@protonmail.com>
2021-03-02 17:53:36 -0300

    refactor buttons design

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 
+import { FiTwitter } from "react-icons/fi";
+
 import styles from '../styles/components/LevelUpModal.module.css'
 
 export function LevelUpModal() {
@@ -14,8 +16,12 @@ export function LevelUpModal() {
         <strong>Parabéns</strong>
         <p>Você alcançou um novo level.</p>
 
-        <button type="button" onClick={closeLevelUpModal}>
+        <button className={styles.closeButton} type="button" onClick={closeLevelUpModal}>
           <img src="/icons/close.svg" alt="Fechar"/>
+        </button>
+
+        <button className={styles.twitterButton}>
+          Compartilhar no Twitter <FiTwitter style={{ marginLeft: '0.5rem' }} />
         </button>
       </div>
     </div>
